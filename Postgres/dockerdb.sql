f7afe401b9a87848b186000a9fe20360c1033954
Chirag Rathi <chiragrathi111@gmail.com>
2026-08-07 18:11:45 +0530

    clean up

--- Postgres/dockerdb.sql
+++ Postgres/dockerdb.sql
@@ -12,3 +12,11 @@
 
 
 * psql -U postgres -d realmeds -f backup.sql  (terminal running specific sql file)
+
+# Run the sql file on the container :-
+
+* docker ps
+
+* docker cp database.sql postgres-db:/tmp/database.sql
+
+* docker exec -it postgres-db psql -U postgres -d realmeds -f /tmp/database.sql
